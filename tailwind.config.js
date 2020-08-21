32c7a918eb9a35dc73d0048f8256be57ff29f9bf
javed432 <javed.akhtar432@gmail.com>
2020-08-21 21:24:16 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter"],
+      },
+      fontSize: {
+        "2.5xl": "1.75rem",
+        "3.5xl": "2rem",
+        "4.5xl": "2.5rem",
+        "6.37xl": "3.375rem",
+        "7xl": "4.5rem",
+      },
+      colors: {
+        primary: "#3ccf91",
+        primaryTwo: "#ff934f",
+        primaryThree: "#5132c0",
+        bgColor: "#12141d",
+        bgColorTwo: "#ffffff0d",
+        stepsColor: "#ffffff1a",
+        textColor: "#ffffff80",
+      },
+      borderRadius: {
+        twelve: "12px",
+      },
+      margin: {
+        "4.5": "4.5rem",
+        "9": "2.25rem",
+        "38": "9rem",
+      },
+      lineHeight: {
+        "7.8xl": "1.875rem",
+        fortyFour: "2.75rem",
+        "6.37xl": "3.375rem",
+        "11": "4.5rem",
+      },
+      letterSpacing: {
+        minusTwo: "-0.02em",
+        minusOne: "-0.01em",
+      },
+      padding: {
+        "2.5": "0.57rem",
+        "9": "2.25rem",
+        "26": "6.75rem",
+      },
+      maxWidth: {
+        sectionWrapper: "1296px",
+        heroContent: "541px",
+        formWidth: "612px",
+        newsletterContent: "824px",
+        clientContent: "964px",
+        maxContent: "max-content",
+      },
+      width: {
+        "9": "2.25rem",
+      },
+      height: {
+        btnHeight: "60px",
+        "9": "2.25rem",
+      },
+      minHeight: {
+        gridHeight: "528px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
